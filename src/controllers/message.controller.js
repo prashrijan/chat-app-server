99dd682e7112b24dee91734c821c55fda0aeb963
Prashrijan <shresthaprashrijan@gmail.com>
2025-11-03 22:37:11 +1100

    complete get messages controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import { Message } from "../models/messages.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -24,5 +25,35 @@ export const getUsersForSidebar = async (req, res) => {
         return res
             .status(500)
             .json(new ApiError(500, "Internal Server Errror"));
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const myId = req.user._id;
+        const { id: userToChatId } = req.params;
+
+        if (!userToChatId) {
+            return res
+                .status(404)
+                .json(new ApiError(404, "User to chat with id is required"));
+        }
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: userToChatId, receiverId: myId },
+                {
+                    senderId: myId,
+                    receiverId: userToChatId,
+                },
+            ],
+        });
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, messages, "Messages found succesfully"));
+    } catch (error) {
+        console.log("Error in getMessages: ", error);
+        return res.status(500).json(new ApiError(500, "Internal Server Error"));
     }
 };
--- src/routes/messages.route.js
+++ src/routes/messages.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+    getMessages,
+    getUsersForSidebar,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.route("/users").get(authenticateUser, getUsersForSidebar);
+router.route("/messages/:id").get(authenticateUser, getMessages);
 
 export default router;
